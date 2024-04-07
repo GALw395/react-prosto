336330d48c393a68de4b6981187c02ad6e8e4265
GALw395 <galw395@mail.ru>
2024-04-07 17:58:43 +0300

    react-prosto-2

--- src/App.tsx
+++ src/App.tsx
@@ -25,20 +25,33 @@ const Rating = () => {
 
 const Star = () => {
   return (
-    <div>star</div>   
+    <div>star</div>
   )
 }
 
 const Accordion = () => {
-  return (<div>
+  return (
+    <div>
+      <AccordionTitle />
+      <AccordionBody />
+    </div>
+
+  )
+}
+
+const AccordionTitle = () => {
+  return (
     <h3>Menu</h3>
+  )
+}
+
+const AccordionBody = () => {
+  return (
     <ul>
       <li>1</li>
       <li>2</li>
       <li>3</li>
     </ul>
-  </div>
-    
   )
 }
 
