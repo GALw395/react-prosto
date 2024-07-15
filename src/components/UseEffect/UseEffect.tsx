42e53a3829c78bc74bf0e6f372b7efe74143ac16
GALw395 <galw395@mail.ru>
2024-07-15 19:06:13 +0300

    react-prosto-22 - useEffect, settimeout, setinterval

--- src/components/UseEffect/UseEffect.tsx
+++ src/components/UseEffect/UseEffect.tsx
@@ -10,15 +10,20 @@ export const UseEffect = () => {
     console.log('simpleExample')
 
     useEffect(()=>{
-        console.log('useEffect') 
-        document.title = counter.toString()
+        // setInterval( () => {
+        //     console.log(counter)            
+        //     setCounter(state => state + 1)
+        // }, 1000 )
+        // setTimeout(()=>{
+        //     document.title = counter.toString()
+        // }, 2000)
         //    сайдэффекты: 
         // api.getUser().then('')   - запросы на сервер(asinc)
         // setInterval              - установка интервалов(asinc)
         // indexedDB                - работа с базой данных
         // document.getElementId    - обращение напрямую к DOM-элементу
         // document.title = 'User ' - изменение напрямую в DOM-элементе
-    }, [counter])
+    }, []) // [counter] - перерисовка в зависимости от "counter". [] - один раз в начале. Если ничего нет, то перерисовывай всегда!!!
     
     return <>
         Hello, {counter}
